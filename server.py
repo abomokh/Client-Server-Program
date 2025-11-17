64be9194fc25c17c8fd255be37e68932904da28d
ibrahim <abomokh@mail.tau.ac.il>
2025-11-17 17:50:12 +0200

    bulding the server basic structure

--- server.py
+++ server.py
@@ -2,18 +2,37 @@
 
 import socket
 
-HOST = ""  # All computer's network interfaces
-PORT = 1337  # Port to listen on (non-privileged ports are > 1023)
+HOST            = ""    # All computer's network interfaces
+PORT            = 1337  # Port to listen on (non-privileged ports are > 1023)
+back_log        = 5
+SELECT_TIMEOUT  = 10
+BUFF_SIZE       = 4
+soc_to_msg      = {}
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSock:
-    serverSock.bind((HOST, PORT))
-    serverSock.listen()
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as listenSoc:
+    listenSoc.bind((HOST, PORT))
+    listenSoc.listen(back_log)
     print("socket is listening...")
-    connectionSock, addr = serverSock.accept()
-    with connectionSock:
-        print(f"Connected by {addr}")
-        while True:
-            data = connectionSock.recv(1024)
-            if not data:
-                break
-            connectionSock.send(data)
+
+    while(True):
+        readable, _, _ = select(
+            rlist=[listenSoc]
+            wlist = []
+            xlist = []
+            timeout = SELECT_TIMEOUT
+        )
+        
+        if listenSoc in readable:
+            clientSoc, clientAddr = listenSoc.accept()
+            soc_to_message[clientSoc] = b""
+
+        for soc in readable:
+            soc_to_message[soc] += soc.recv(BUFF_SIZE)
+            if soc_to_message[soc][-1] = '\n':
+                
+                # end of message. handle it
+                handler(soc, soc_to_message[soc])
+
+                # delete the message
+                soc_to_message[soc] = b""
